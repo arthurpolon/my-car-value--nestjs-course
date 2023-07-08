cc70115c7633cb9ada751512da08cb2c4b2a948e
Arthur Polom <arthurpolon@hotmail.com>
2023-07-08 10:30:51 -0300

    add cookies manipulation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Query,
   Delete,
   NotFoundException,
+  Session,
 } from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UsersService } from './users.service'
@@ -25,14 +26,32 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  async whoAmI(@Session() session: any) {
+    const user = await this.userService.findOne(session.userId)
+
+    if (!user) throw new NotFoundException('user not found')
+
+    return user
+  }
+
+  @Post('signout')
+  async signOut(@Session() session: any) {
+    session.userId = null
+  }
+
   @Post('signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password)
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Post('signin')
-  signin(@Body() body: SigninUserDto) {
-    return this.authService.signin(body.email, body.password)
+  async signin(@Body() body: SigninUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Get('/:id')
