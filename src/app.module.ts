55365ae36c38e9ee1603b72191030dcf56565cec
Arthur Polom <arthurpolon@hotmail.com>
2023-07-11 17:41:44 -0300

    add current-user middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,12 +25,6 @@ const cookieSession = require('cookie-session')
         synchronize: true,
       }),
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
     UsersModule,
     ReportsModule,
   ],
