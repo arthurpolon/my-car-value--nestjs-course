119abbfbb1fd86b810050efa76e9947b43022734
Arthur Polom <arthurpolon@hotmail.com>
2023-07-14 01:53:19 -0300

    add production db config

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -6,13 +6,42 @@ dotenv.config({
   path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`),
 })
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: process.env.DB_NAME || 'db.sqlite',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  migrations: ['dist/db/migrations/*.js'],
-  migrationsRun: true,
-}
+export const dataSourceOptions: DataSourceOptions = (() => {
+  switch (process.env.NODE_ENV) {
+    case 'development': {
+      return {
+        type: 'sqlite',
+        database: process.env.DB_NAME || 'db.sqlite',
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: true,
+      }
+    }
+    case 'test': {
+      return {
+        type: 'sqlite',
+        database: process.env.DB_NAME || 'db.sqlite',
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: true,
+      }
+    }
+    case 'production': {
+      return {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: true,
+      }
+    }
+    default:
+      throw new Error('unknown environment')
+  }
+})()
 
 const dataSource = new DataSource(dataSourceOptions)
 
